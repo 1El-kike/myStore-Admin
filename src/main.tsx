ca73e02290634211fe008f298eb924384219ee11
Enrique Grass Porras <enriquegrassporras@gmail.com>
2025-01-13 21:38:46 +0100

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,18 @@ import { App } from "./App";
 import "./index.css";
 import { AuthProvider } from "./utils/AuthContext";
 import { Routers } from "./router/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 //setupAxios(axios)
 //Chart.register(...registerables)
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
- <React.StrictMode>
-     <AuthProvider>
-       <Routers />
-     </AuthProvider>
-   </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <Routers />
+    </AuthProvider>
+   {/*  <ReactQueryDevtools initialIsOpen={true} /> */}
+  </QueryClientProvider>
 );
